fbd01d710a9cac61302af5d0b63a9033f0fec542
Cyan903 <cyanp0515@gmail.com>
2022-04-23 22:47:11 -0400

    [+] feat: Upload YouTube video logic
    With additional configs.

--- src/api/youtube.ts
+++ src/api/youtube.ts
@@ -1,9 +1,14 @@
-import { Secrets, OAuth } from "../types/auth";
-import { writeFileSync } from "fs";
+import { Secrets, OAuth, Video } from "../types/auth";
+import { VideoResponse } from "../types/res";
+import { writeFileSync, createReadStream, readFileSync } from "fs";
 import { google } from "googleapis";
 import { spawn } from "child_process";
 import readlineSync from "readline-sync";
 import consola from "consola";
+
+type resData = {
+    data: VideoResponse;
+};
 
 const OAuth2 = google.auth.OAuth2;
 const API_SCOPE = ["https://www.googleapis.com/auth/youtube.upload"];
@@ -13,11 +18,12 @@ const CATEGORY = {
     ScienceTechnology: 28,
 };
 
-export function genAuth(path: string, secrets: Secrets) {
+export function genAuth(authPath: string, secretPath: string) {
+    const secret = loadSecret(secretPath);
     const client: any = new OAuth2(
-        secrets.installed.client_id,
-        secrets.installed.client_secret,
-        secrets.installed.redirect_uris[0]
+        secret.installed.client_id,
+        secret.installed.client_secret,
+        secret.installed.redirect_uris[0]
     );
 
     const url = client.generateAuthUrl({
@@ -27,7 +33,7 @@ export function genAuth(path: string, secrets: Secrets) {
 
     // Start webserver for Google API
     // prettier-ignore
-    spawn("node", ["./dist/api/server.js", secrets.installed.redirect_uris[0]],
+    spawn("node", ["./dist/api/server.js", secret.installed.redirect_uris[0]],
         {
             stdio: ["ignore"],
             detached: true,
@@ -42,14 +48,94 @@ export function genAuth(path: string, secrets: Secrets) {
         readlineSync.question("\n\nCode: "),
         (err: Error, token: OAuth) => {
             if (err) {
-                console.log("Error while trying to retrieve access token", err);
+                consola.info(
+                    "Error while trying to retrieve access token",
+                    err
+                );
                 return;
             }
-                        
-            consola.info("Saved auth data to", path);
-            writeFileSync(path, JSON.stringify(token));
+
+            consola.info("Saved auth data to", authPath);
+            writeFileSync(authPath, JSON.stringify(token));
 
             client.credentials = token;
+        }
+    );
+}
+
+export function loadAuth(authPath: string, secretPath: string) {
+    const secret = loadSecret(secretPath);
+    const client: any = new OAuth2(
+        secret.installed.client_id,
+        secret.installed.client_secret,
+        secret.installed.redirect_uris[0]
+    );
+
+    client.credentials = JSON.parse(
+        readFileSync(authPath, {
+            encoding: "utf8",
+        })
+    );
+
+    return client;
+}
+
+function loadSecret(path: string): Secrets {
+    return JSON.parse(
+        readFileSync(path, {
+            encoding: "utf-8",
+        })
+    );
+}
+
+export function upload(auth: any, video: Video) {
+    const service: any = google.youtube("v3");
+
+    service.videos.insert(
+        {
+            auth,
+            part: "snippet,status",
+            requestBody: {
+                snippet: {
+                    title: video.title,
+                    description: video.description,
+                    tags: video.tags,
+                    categoryId: CATEGORY.ScienceTechnology,
+                    defaultLanguage: "en",
+                    defaultAudioLanguage: "en",
+                },
+                status: {
+                    privacyStatus: "private",
+                },
+            },
+            media: {
+                body: createReadStream(video.vid),
+            },
+        },
+        (err: Error, response: resData) => {
+            if (err) {
+                consola.error("The API returned an error: " + err);
+                return;
+            }
+            consola.info(response.data);
+
+            consola.info("Video uploaded. Uploading the thumbnail now.");
+            service.thumbnails.set(
+                {
+                    auth: auth,
+                    videoId: response.data.id,
+                    media: {
+                        body: createReadStream(video.thumb),
+                    },
+                },
+                (err: Error, response: resData) => {
+                    if (err) {
+                        consola.error("The API returned an error: " + err);
+                        return;
+                    }
+                    consola.info(response.data);
+                }
+            );
         }
     );
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-import { genAuth } from "./api/youtube";
+import { genAuth, loadAuth, upload } from "./api/youtube";
 
-genAuth("./auth.json", {
-    installed: {
-        client_id: "",
-        client_secret: "",
-        redirect_uris: ["http://localhost:8080/oauth2callback"],
-        auth_uri: "https://accounts.google.com/o/oauth2/auth",
-        token_uri: "https://accounts.google.com/o/oauth2/token",
-    },
+// Generate Auth
+(() => {
+    // First argument is auth output
+    genAuth("./config/auth.json", "./config/secret.json");
+});
+
+
+// Upload Video
+(() => {
+    const auth = loadAuth("./config/auth.json", "./config/secret.json");
+
+    upload(auth, {
+            title: "Very cool",
+            description: "Upload test",
+            tags: ["wow"],
+            thumb: "./tmp/res/thumb.png",
+            vid: "./tmp//res/hehe.mp4",
+        }
+    );
 });
